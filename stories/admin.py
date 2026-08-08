6d0f3c0c4ddfc81828e99ce1ed11134d6aea438a
Abhishek Mohite <abhishekmohite421@gmail.com>
2026-08-08 10:59:25 +0530

    feat: improve story admin and explore django orm

--- stories/admin.py
+++ stories/admin.py
@@ -2,4 +2,23 @@ from django.contrib import admin
 
 from .models import Story
 
-admin.site.register(Story)
+@admin.register(Story)
+class StoryAdmin(admin.ModelAdmin):
+    list_display = [
+        "title", 
+        "author", 
+        "status", 
+        "created_at", 
+        "updated_at" 
+        ]
+    search_fields = [
+        "title", 
+        "content"
+        ]
+    list_filter = [
+        "status", 
+        "created_at"
+        ]
+    prepopulated_fields = {
+        "slug": ("title",),
+    }
