d53b6257e4467b60c7afe38d95835e46dfb8a36c
Abhishek Mohite <abhishekmohite421@gmail.com>
2026-08-10 22:30:26 +0530

    feat: add story URL routing and views

--- stories/views.py
+++ stories/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def story_list(request):
+    stories = """
+    story1
+    story2
+    story3
+    """
+    return HttpResponse(stories)
+
+
+def story_detail(request, story_id):
+    story = f"Story {story_id}"
+    return HttpResponse(story)
