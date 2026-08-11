cabe69e15ee40c4ed6ca80b21b76660e501324eb
Abhishek Mohite <abhishekmohite421@gmail.com>
2026-08-11 22:25:02 +0530

    feat: query stories from database in views

--- stories/views.py
+++ stories/views.py
@@ -1,15 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Story
 
 def story_list(request):
-    stories = """
-    story1
-    story2
-    story3
-    """
-    return HttpResponse(stories)
+    status = request.GET.get('status')
+    if status:
+        stories = Story.objects.filter(status=status)
+    else:
+        stories = Story.objects.all()
+    stories_output = ""
+    for story in stories:
+        stories_output += f"<p>{story.id}: {story.title}</p>"
+
+    return HttpResponse(stories_output)
 
 
 def story_detail(request, story_id):
-    story = f"Story {story_id}"
-    return HttpResponse(story)
+
+    try:
+        story = Story.objects.get(id=story_id)
+    except Story.DoesNotExist:
+        return HttpResponse("Story not found", status=404)
+    detail = f"<p>Title: {story.title}</p><p>Author: {story.author}</p><p>Status: {story.status}</p><p>Content: {story.content}</p>"
+    return HttpResponse(detail)
