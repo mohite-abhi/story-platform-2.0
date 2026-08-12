0dee96094cfc7c7467363a0213b307766062aaf9
Abhishek Mohite <abhishekmohite421@gmail.com>
2026-08-12 22:09:44 +0530

    feat: add templates for story list and story detail

--- stories/views.py
+++ stories/views.py
@@ -8,18 +8,14 @@ def story_list(request):
         stories = Story.objects.filter(status=status)
     else:
         stories = Story.objects.all()
-    stories_output = ""
-    for story in stories:
-        stories_output += f"<p>{story.id}: {story.title}</p>"
 
-    return HttpResponse(stories_output)
+    return render(request, 'stories/story_list.html', {'stories': stories})
 
 
 def story_detail(request, story_id):
-
     try:
         story = Story.objects.get(id=story_id)
     except Story.DoesNotExist:
         return HttpResponse("Story not found", status=404)
-    detail = f"<p>Title: {story.title}</p><p>Author: {story.author}</p><p>Status: {story.status}</p><p>Content: {story.content}</p>"
-    return HttpResponse(detail)
+        
+    return render(request, 'stories/story_detail.html', {'story': story})
