c60d561d80f9739e9637e7d138b7f323ce3d9e6c
Abhishek Mohite <abhishekmohite421@gmail.com>
2026-08-29 19:00:20 +0530

    feat: add story list and detail views

--- stories/models.py
+++ stories/models.py
@@ -7,7 +7,7 @@ class Story(models.Model):
     author = models.ForeignKey(
         settings.AUTH_USER_MODEL, 
         on_delete=models.CASCADE)
-    content = models.TextField()
+    content = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=100)
