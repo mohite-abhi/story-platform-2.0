600e4a3d5184a92d7806b27def948f843e7dd3ce
Abhishek Mohite <abhishekmohite421@gmail.com>
2026-09-07 22:43:47 +0530

    feat: implement login/logout

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib.auth import authenticate, login, logout
 
-# Create your views here.
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('story_list')  # Redirect to the story list page after successful login
+        else:
+            # If authentication fails, you can return an error message
+            return render(request, 'accounts/login.html', {'error': 'Invalid credentials'})
+    return render(request, 'accounts/login.html')
+
+def logout_view(request):
+    if request.method == 'POST':
+        # Handle logout logic here
+        logout(request)
+    elif request.method == 'GET':
+        return render(request, 'accounts/logout.html')  # Render a logout confirmation page for GET requests
+        
+    return redirect('story_list')  # Redirect to the story list page if not a POST request
